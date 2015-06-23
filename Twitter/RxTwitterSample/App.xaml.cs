c671611c8f8c48540740193ec5a8f38dcb34b665
Leif Battermann <leif.battermann@webrunners.de>
2015-06-23 11:39:40 +0200

    exception handling

--- Twitter/RxTwitterSample/App.xaml.cs
+++ Twitter/RxTwitterSample/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using RxTwitterSample.viewmodels;
 using TwitterApi;
 
@@ -16,6 +17,12 @@ namespace RxTwitterSample
             var vm = new MainWindowViewModel(TweetAnalysis.Classify , twitter.AllTweetsAbout);
             mainWindow.DataContext = vm;
             mainWindow.Show();
+        }
+
+        private void Application_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show(e.Exception.Message, "Error", MessageBoxButton.OK, MessageBoxImage.Warning);
+            Environment.Exit(1);
         }
     }
 }
